bfabbf02b57c5580097ec5b75471436a53d2c7d6
R.K.Menaga <menagark2005@gmail.com>
2025-11-10 18:02:05 +0530

    Initialize i18n configuration for localization

---
+++ i18n.jsx
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+i18n
+  // loads translations from your /public/locales folder
+  .use(Backend)
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    supportedLngs: ['en', 'hi', 'ta'],
+  });
+
+export default i18n;
